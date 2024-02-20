37ba0656846420a5540a94d63221090cb216a2a5
JanPalasek <jan.palasek@seznam.cz>
2024-02-20 22:03:06 +0000

    Improved schema.

--- src/hgpt/spiders/_llm/_detail.py
+++ src/hgpt/spiders/_llm/_detail.py
@@ -9,8 +9,8 @@ class Location(BaseModel):
     """Location of the real estate."""
 
     address: str = Field(default=None, description="Address of the property. If it is not specified, use null.")
-    floor_level: int = Field(default=None, description="Floor level of the real estate (if there is any).")
-    total_floor_levels: int = Field(
+    floor_level: int | None = Field(default=None, description="Floor level of the real estate (if there is any).")
+    total_floor_levels: int | None = Field(
         default=None,
         description="How many total floors does the building have. Fill only if the information is present in the text.",
     )
@@ -22,20 +22,20 @@ class Size(BaseModel):
     floor_area: float = Field(
         description="Total floor area of the real estate. This area typically also includes size of loggia, size of built-in furniture. Typically in squared meters."
     )
-    usable_floor_area: float = Field(
+    usable_floor_area: float | None = Field(
         description="Usable floor area of the real estate. This information typically excludes some in-built furniture etc."
     )
-    balcony_area: float = Field(
+    balcony_area: float | None = Field(
         default=0,
         description="Size of the balcony. If the real estate does not have a balcony, must be 0.",
     )
-    loggia_area: float = Field(
+    loggia_area: float | None = Field(
         default=0, description="Size of the loggia. If the real estate does not have a loggia, must be 0."
     )
-    cellar_area: float = Field(
+    cellar_area: float | None = Field(
         default=0, description="Size of the cellar. If the real estate does not have a cellar, must be 0."
     )
-    pantry_area: float = Field(
+    pantry_area: float | None = Field(
         default=0, description="Size of the pantry. Should only contain number if property has a pantry."
     )
 
@@ -43,9 +43,9 @@ class Size(BaseModel):
 class State(BaseModel):
     """Information about what shape the property is in."""
 
-    new_building: bool = Field(description="If the real estate is new, then this mut be true.")
-    built_year: int = Field(description="Year when the property was built.")
-    reconstructed_year: int = Field(description="Year when the property was last reconstructed.")
+    new_building: bool | None = Field(description="If the real estate is new, then this mut be true.")
+    built_year: int | None = Field(description="Year when the property was built.")
+    reconstructed_year: int | None = Field(description="Year when the property was last reconstructed.")
 
 
 class RealEstate(BaseModel):
@@ -54,10 +54,10 @@ class RealEstate(BaseModel):
     """
 
     price: float = Field(description="Price of the real-estate property.", required=True, ge=0)
-    additional_fees: bool = Field(
+    additional_fees: bool | None = Field(
         description="Sometimes the price is not complete and it is also necessary to pay for real estate agent etc. If so, this field must be true.",
     )
-    monthly_fees: float = Field(
+    monthly_fees: float | None = Field(
         default=None, description="Monthly fees that the owners are expected to pay in this property."
     )
     layout: str = Field(description="Layout of the real estate. Typically something like '3+kk', '4+1' etc.")
